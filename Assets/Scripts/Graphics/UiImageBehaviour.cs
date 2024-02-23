955b6fb270acb08daad379db9d8587766262b22b
Remi Bodin <remi.bodin@gmail.com>
2024-02-23 17:53:43 +0100

    Add a behaviour to use a SpriteAsset on an UI.Image

---
+++ Assets/Scripts/Graphics/UiImageBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+
+using Cgw.Assets;
+
+namespace Cgw.Graphics
+{
+    public class UiImageBehaviour : AssetBehaviour<SpriteAsset>
+    {
+        private Image m_renderer;
+
+        private void Awake()
+        {
+            m_renderer = gameObject.GetOrAddComponent<Image>();
+        }
+
+        protected override void AssetUpdated()
+        {
+            m_renderer.sprite = Asset.Sprite;
+        }
+    }
+}
