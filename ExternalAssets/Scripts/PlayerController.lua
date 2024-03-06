caf8caa855f67be0f66789cc743be8b4b0f6378b
Rémi Bodin <remi.bodin@gmail.com>
2024-03-06 23:14:36 +0100

    Update player controller
    Cooldown on landing

--- ExternalAssets/Scripts/PlayerController.lua
+++ ExternalAssets/Scripts/PlayerController.lua
@@ -1,11 +1,11 @@
 import 'UnityEngine'
 
 local Life = 3;
-local Speed = 3.0;
-local AirSpeed = 3.5;
+local Speed = 1.5;
+local AirSpeed = 2.5;
 local AttackRange = 1.5;
 local AttackPower = 1.0;
-local JumpTime = 0.2;
+local JumpTime = 0.85;
 local AttackTime = 1.0;
 local JumpForce = 8.0;
 
@@ -17,21 +17,39 @@ function Update()
         end
     end
 
-    if (Input.GetKey("left")) then
-        if (this.OnGround) then
+    if (this.OnGround and this.JumpCooldown == 0.0) then
+        if (Input.GetKey("left")) then
             this:Move(Speed, -1.0)
-        else
-            this:Move(AirSpeed, -1.0)
+        end
+        if (Input.GetKey("right")) then
+            this:Move(Speed, 1.0)
         end
     end
 
-    if (Input.GetKey("right")) then
-        if (this.OnGround) then
-            this:Move(Speed, 1.0)
-        else
+    if (this.OnGround == false) then
+        if (Input.GetKey("left")) then
+            this:Move(AirSpeed, -1.0)
+        end
+        if (Input.GetKey("right")) then
             this:Move(AirSpeed, 1.0)
         end
     end
+
+    -- if (Input.GetKey("left")) then
+    --     if (this.OnGround and this.JumpCooldown == 0.0) then
+    --         this:Move(Speed, -1.0)
+    --     else if (this.OnGround == false) then
+    --         this:Move(AirSpeed, -1.0)
+    --     end
+    -- end
+
+    -- if (Input.GetKey("right")) then
+    --     if (this.OnGround and this.JumpCooldown == 0.0) then
+    --         this:Move(Speed, 1.0)
+    --     else if (this.OnGround == false) then
+    --         this:Move(AirSpeed, 1.0)
+    --     end
+    -- end
 
     if (Input.GetKeyDown("f")) then
         if (this.AttackCooldown == 0.0) then
