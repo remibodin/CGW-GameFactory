ed6868c5af1c16f489b6067fe21292487ec7d709
Damien VEZANT <damien.vezant@gmail.com>
2024-03-07 19:19:54 +0100

    Add audio manager and prefab spawn skeletons

---
+++ Assets/Scripts/Assets/AudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Cgw.Assets
+{
+    public class AudioManager
+    {
+        public static void PlayOneShot(string identifier)
+        {
+
+        }
+
+        public static void PlayLoop(string identifier)
+        {
+
+        }
+    }
+}
