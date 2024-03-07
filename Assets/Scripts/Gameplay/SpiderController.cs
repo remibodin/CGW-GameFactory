3ec090a477beaaa4cde0139f2fb9861059dfe025
Damien VEZANT <damien.vezant@gmail.com>
2024-03-07 15:10:09 +0100

    Added base of spider controller in lua and updated Level-Test prefab accordingly

---
+++ Assets/Scripts/Gameplay/SpiderController.cs
@@ -0,0 +1,29 @@
+using Cgw.Assets;
+using Cgw.Scripting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cgw.Gameplay
+{
+    public class SpiderController : LuaEnvItem
+    {
+        void Start()
+        {
+            var scriptBehaviour = gameObject.AddComponent<LuaBehaviour>();
+            scriptBehaviour.OnAssetUpdated += ScriptBehaviour_OnAssetUpdated;
+            scriptBehaviour.Script = ResourcesManager.Get<LuaScript>("Scripts/SpiderController");
+        }
+
+        private void ScriptBehaviour_OnAssetUpdated(LuaInstance instance)
+        {
+            instance["this"] = this;
+        }
+
+        public void MoveFollow(Vector3 destination, float speed)
+        {
+            transform.position = Vector3.Lerp(transform.position, destination, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ ExternalAssets/Scripts/SpiderController.lua
@@ -0,0 +1,26 @@
+import 'UnityEngine'
+
+local FollowSpeed = 2.0;
+local FollowDistance = 0.5;
+local FollowHeight = 0.3
+
+function Start()
+    local targetPosition = player.transform.position + Vector3.up * FollowHeight
+    if (player.Facing.x > 0.0) then
+        targetPosition = targetPosition + Vector3.left * FollowDistance
+    else
+        targetPosition = targetPosition + Vector3.right * FollowDistance
+    end
+    this.transform.position = targetPosition
+end
+
+function Update()
+    local targetPosition = player.transform.position + Vector3.up * FollowHeight
+    if Vector3.Distance(this.transform.position, targetPosition) > FollowDistance then
+        if this.transform.position.x < targetPosition.x then
+            this:MoveFollow(targetPosition + Vector3.left * FollowDistance, FollowSpeed)
+        else
+            this:MoveFollow(targetPosition + Vector3.right * FollowDistance, FollowSpeed)
+        end
+    end
+end
