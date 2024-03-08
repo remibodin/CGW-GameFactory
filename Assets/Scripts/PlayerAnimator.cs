5fb6731cbf89d926eb204589857301c6eae1a58a
Rémi Bodin <remi.bodin@gmail.com>
2024-03-08 18:15:08 +0100

    Update player animation controller
    Add onGround in Animator

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,6 +4,7 @@ using Cgw.Assets;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    private Cgw.Gameplay.Player m_player;
     private Animator m_animator;
     private SpriteRenderer m_renderer;
     private Vector3 m_lastFramePosition;
@@ -13,20 +14,22 @@ public class PlayerAnimator : MonoBehaviour
     {
         m_animator = GetComponent<Animator>();
         m_renderer = GetComponent<SpriteRenderer>();
+        m_player = GetComponent<Cgw.Gameplay.Player>();
     }
 
-    void Update()
+    void LateUpdate()
     {
         var currentPosition = transform.position;
         m_motion = currentPosition - m_lastFramePosition;
         m_lastFramePosition = transform.position;
 
-        m_animator.SetFloat("AbsH", Mathf.Abs(m_motion.x) * 100f);
+        m_animator.SetFloat("AbsH", Mathf.Abs(m_player.Motion.x) * 100f);
         m_animator.SetFloat("V", m_motion.y * 100f);
+        m_animator.SetBool("OnGround", m_player.OnGround);
 
         if (m_motion.x != 0)
         {
-            m_renderer.flipX = m_motion.x < 0;
+            m_renderer.flipX = m_player.Motion.x < 0;
         }
     }
 }
