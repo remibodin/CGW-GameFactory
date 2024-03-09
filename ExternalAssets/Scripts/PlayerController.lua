21b93b09c3712257de6d86842a07c23180c8a921
ArmandGemeux <armandgemeux@hotmail.fr>
2024-03-09 02:37:31 +0100

    SlopeTest scene pour Damien + PlayerController adjust

--- ExternalAssets/Scripts/PlayerController.lua
+++ ExternalAssets/Scripts/PlayerController.lua
@@ -1,10 +1,10 @@
 import 'UnityEngine'
 
 local Life = 3;
-local Speed = 0.75;
-local AirSpeed = 1.2;
-local JumpTime = 0.65;
-local JumpForce = 6.0;
+local Speed = 2.5;
+local AirSpeed = 2.5;
+local JumpTime = 0.3;
+local JumpForce = 9.0;
 
 local AttackTime = 1.0;
 local DamageTime = 0.8;
