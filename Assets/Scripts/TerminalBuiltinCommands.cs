40af00ee3953d5d159ad0ec70d70334bfe94cba0
Remi Bodin <remi.bodin@gmail.com>
2024-03-30 00:27:08 +0100

    Add terminal command to spawn prefab at runtime

--- Assets/Scripts/TerminalBuiltinCommands.cs
+++ Assets/Scripts/TerminalBuiltinCommands.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 using Cgw.Assets;
 using Cgw.Localization;
+using Cgw.Gameplay;
 
 namespace Cgw
 {
@@ -24,6 +25,33 @@ namespace Cgw
         public static void Help(string p_args)
         {
             Debug.Log($"<b>Commands list</b>:\n{string.Join('\n', Terminal.Instance.CommandsName)}");
+        }
+
+        [TermCommand]
+        public static void Spawn(string p_args)
+        {
+            Player player = GameObject.FindFirstObjectByType<Player>();
+            if (player == null)
+            {
+                UnityEngine.Debug.LogWarning("Player not found in scene");
+                return;
+            }
+            GameObject go = null;
+            switch (p_args)
+            {
+                case "mu" : 
+                    go = Resources.Load("Enemies/Mushroom") as GameObject;
+                break;
+                default:
+                    go = Resources.Load(p_args) as GameObject;
+                break;
+            }
+            if (go == null)
+            {
+                UnityEngine.Debug.LogWarning("Resource not found");
+                return;
+            }
+            GameObject.Instantiate(go, player.transform.position + Vector3.right * 3, Quaternion.identity);
         }
     }
 }
