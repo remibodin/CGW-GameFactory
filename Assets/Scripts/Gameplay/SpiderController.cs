e54cf5885de0c66e06fc293692a42d14f509cf17
Lukior <Lukior@users.noreply.github.com>
2025-02-22 12:56:42 +0100

    UnLUAfication of SpiderController

--- Assets/Scripts/Gameplay/SpiderController.cs
+++ Assets/Scripts/Gameplay/SpiderController.cs
@@ -1,27 +1,48 @@
 using Cgw.Assets;
 using Cgw.Scripting;
+using UnityEditor.Rendering.Analytics;
 using UnityEngine;
 
 namespace Cgw.Gameplay
 {
-    public class SpiderController : LuaEnvItem
+    public class SpiderController : MonoBehaviour
     {
+        public float Life = 3;
+        public float LifePerLaunch = 0.5f;
+
+        public float FollowSpeed = 2.0f;
+        public float FollowDistance = 0.1f;
+        public float FollowHeight = 0.6f;
+
+        public bool CanLaunch = true;
+        public float LaunchRange = 6.0f;
+        public float LaunchSpeed = 4.0f;
+
         public ContactFilter2D ContactFilter;
         public float TouchTime = 3.0f;
-        public float TouchSpeedMultiplier = 0.3f;
+        public float TouchSpeedMultiplier = 0.1f;
 
-        void Start()
+        public bool HasTarget = false;
+        public Enemy Target = null;
+        public Vector3 TargetPoint = Vector3.zero;
+
+        public void Start()
         {
-            var scriptBehaviour = gameObject.AddComponent<LuaBehaviour>();
-            scriptBehaviour.OnAssetUpdated += OnAssetUpdate;
-            scriptBehaviour.Script = ResourcesManager.Get<LuaScript>("Scripts/SpiderController");
-        }
+            var player = Player.PlayerInstance;
 
-        protected override void OnAssetUpdate(LuaInstance instance)
-        {
-            base.OnAssetUpdate(instance);
-
-            instance["this"] = this;
+            if (player != null)
+            {
+                var targetPosition = player.transform.position + Vector3.up * FollowHeight;
+                if (player.Facing.x > 0.0f)
+                {
+                    targetPosition += Vector3.left * FollowDistance;
+                }
+                else
+                {
+                    targetPosition += Vector3.right * FollowDistance;
+                }
+                transform.position = targetPosition;
+            }
         }
 
         public void MoveFollow(Vector3 destination, float speed)
@@ -46,7 +67,23 @@ namespace Cgw.Gameplay
 
         public void Launch()
         {
-            m_Instance.Call("Launch");
+            if (CanLaunch && Life > 0)
+            {
+                var player = Player.PlayerInstance;
+                var enemy = CheckLaunch(player.transform.position + Vector3.up * FollowHeight, player.Facing, LaunchRange);
+                if (enemy != null)
+                {
+                    HasTarget = true;
+                    Target = enemy;
+                    Life = Life - LifePerLaunch;
+                }
+                else
+                {
+                    HasTarget = false;
+                    Target = null;
+                    TargetPoint = player.transform.position + player.Facing * LaunchRange;
+                }
+            }
         }
 
         public void OnCollisionEnter2D(Collision2D collision)
@@ -56,7 +93,7 @@ namespace Cgw.Gameplay
                 var enemy = collision.collider.GetComponent<Enemy>();
                 if (enemy != null)
                 {
-                    m_Instance.Call("OnCollisionWithEnemy");
+                    HasTarget = false;
                     enemy.OnCollisionWithSpider();
                 }
             }
@@ -69,8 +106,40 @@ namespace Cgw.Gameplay
                 var enemy = collision.GetComponent<Enemy>();
                 if (enemy != null)
                 {
-                    m_Instance.Call("OnCollisionWithEnemy");
+                    HasTarget = false;
                     enemy.OnCollisionWithSpider();
+                }
+            }
+        }
+
+        public void Update()
+        {
+            var player = Player.PlayerInstance;
+            var targetPosition = player.transform.position + Vector3.up * FollowHeight;
+            if (!HasTarget && Vector3.Distance(transform.position, targetPosition) > FollowDistance)
+            {
+                if (transform.position.x < targetPosition.x)
+                {
+                    MoveFollow(targetPosition + Vector3.left * FollowDistance, FollowSpeed);
+                }
+                else
+                {
+                    MoveFollow(targetPosition + Vector3.right * FollowDistance, FollowSpeed);
+                }
+            }
+            else if (HasTarget)
+            {
+                if (Target != null)
+                {
+                    MoveTowards(Target.transform.position, LaunchSpeed);
+                }
+                else
+                {
+                    MoveTowards(TargetPoint, LaunchSpeed);
+                    if (Vector3.Distance(TargetPoint, transform.position) < 0.1f)
+                    {
+                        HasTarget = false;
+                    }
                 }
             }
         }
