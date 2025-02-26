50ee44de955eec1c3ad16f5998aa92f5e0ce7cc5
Lukior <Lukior@users.noreply.github.com>
2025-02-26 22:24:52 +0100

    Ajout du prefab de souvenir

---
+++ Assets/Scripts/Gameplay/Souvenir.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class Souvenir : MonoBehaviour
+    {
+        public ParticleSystem ParticleSystem;
+
+        public void ActivateDots()
+        {
+            Debug.Log("dots");
+            ParticleSystem.Play();
+        }
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("triggered");
+            if (collision.tag == "Player")
+            {
+                GetComponent<Animator>().SetBool("Activated", true);
+            }
+        }
+    }
+}
